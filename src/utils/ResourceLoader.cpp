8b7128f76a6cdbb51eed3701a57d7dc65424762d
Akshay Agarwal <agarwal.akshay.akshay8@gmail.com>
2017-05-15 13:17:26 +0530

    Make temporary BBitmap cheap to create and fix possble memory leak.
    * Use B_BITMAP_NO_SERVER_LINK flag for creating temp BBitmap object. BBitmaps are expensive and this makes it less costly.
    * Delete the temp BBitmap object before returning dest to avoid possible memory leak.

--- src/utils/ResourceLoader.cpp
+++ src/utils/ResourceLoader.cpp
@@ -17,13 +17,13 @@ BBitmap* LoadVectorIcon(const char* name, int32 iconSize, int32 cropSize)
 	BResources* res = BApplication::AppResources();
 	size_t length = 0;
 	const void* data = res->LoadResource(B_VECTOR_ICON_TYPE, name, &length);
-	BBitmap* temp = new BBitmap(BRect(0, 0, iconSize, iconSize), B_RGBA32);
+	BBitmap* temp = new BBitmap(BRect(0, 0, iconSize, iconSize), B_BITMAP_NO_SERVER_LINK, B_RGBA32);
 	BBitmap* dest = new BBitmap(BRect(0, 0, cropSize, cropSize), B_RGBA32);
 	if (data != NULL &&
 		BIconUtils::GetVectorIcon((uint8*)data, length, temp) == B_OK)
 			if (dest->ImportBits(temp, BPoint(0, 0),
 				BPoint(0, 0), cropSize, cropSize) == B_OK)
-
+				delete temp;
 				return dest;
 
 	delete temp;
