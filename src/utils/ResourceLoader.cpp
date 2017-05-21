e09edb9c92c461a88709936eb7db9fc269a109c8
Akshay Agarwal <agarwal.akshay.akshay8@gmail.com>
2017-05-22 02:48:17 +0530

    Merge multiple if statements into one and style fixes.

--- src/utils/ResourceLoader.cpp
+++ src/utils/ResourceLoader.cpp
@@ -18,14 +18,17 @@ BBitmap* LoadVectorIcon(const char* name, int32 iconSize, int32 cropSize)
 	size_t length = 0;
 	const void* data = res->LoadResource(B_VECTOR_ICON_TYPE, name, &length);
 	BBitmap* temp = new BBitmap(BRect(0, 0, iconSize - 1, iconSize - 1),
-				B_BITMAP_NO_SERVER_LINK, B_RGBA32);
-	BBitmap* dest = new BBitmap(BRect(0, 0, cropSize -1, cropSize - 1), B_RGBA32);
-	if (data != NULL &&
-		BIconUtils::GetVectorIcon((uint8*)data, length, temp) == B_OK)
-			if (dest->ImportBits(temp, BPoint(0, 0),
-				BPoint(0, 0), cropSize, cropSize) == B_OK)
-				delete temp;
-				return dest;
+		B_BITMAP_NO_SERVER_LINK, B_RGBA32);
+	BBitmap* dest = new BBitmap(BRect(0, 0, cropSize - 1, cropSize - 1),
+		B_RGBA32);
+	if (data != NULL
+		&& BIconUtils::GetVectorIcon((uint8*)data, length, temp)
+			== B_OK
+		&& dest->ImportBits(temp, BPoint(0, 0), BPoint(0, 0),
+			cropSize, cropSize) == B_OK)  {
+			delete temp;
+			return dest;
+	}
 
 	delete temp;
 	delete dest;
