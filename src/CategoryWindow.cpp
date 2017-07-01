f723783a03aad41a3bf98f1cd06b8248c661ffb1
Akshay Agarwal <agarwal.akshay.akshay8@gmail.com>
2017-07-02 04:20:11 +0530

    Add CategoryWindow class.

---
+++ src/CategoryWindow.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2017 Akshay Agarwal<agarwal.akshay.akshay8@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+#include "CategoryWindow.h"
+
+#include <LayoutBuilder.h>
+
+
+CategoryWindow::CategoryWindow()
+	:
+	BWindow(BRect(), "Category Manager", B_TITLED_WINDOW,
+		B_AUTO_UPDATE_SIZE_LIMITS)
+{
+
+	CenterOnScreen();
+
+	fMainView = new BView("MainView", B_WILL_DRAW);
+	fMainView->SetViewUIColor(B_PANEL_BACKGROUND_COLOR);
+
+	fCategoryList = new BListView("CategoryList", B_SINGLE_SELECTION_LIST,
+		B_WILL_DRAW);
+
+	fCategoryScroll = new BScrollView("CategoryScroll", fCategoryList,
+		B_WILL_DRAW, false, true);
+	fCategoryScroll->SetExplicitMinSize(BSize(260, 260));
+
+	fCategoryList->SetInvocationMessage(new BMessage(kCategorySelected));
+
+	fAddButton = new BButton("AddButton", "Add", new BMessage(kAddPressed));
+	fCancelButton = new BButton("CancelButton", "Cancel", new BMessage(kCancelPressed));
+
+	fAddButton->SetExplicitMaxSize(BSize(B_SIZE_UNLIMITED, B_SIZE_UNLIMITED));
+	fCancelButton->SetExplicitMaxSize(BSize(B_SIZE_UNLIMITED, B_SIZE_UNLIMITED));
+
+	BLayoutBuilder::Group<>(fMainView, B_VERTICAL)
+		.Add(fCategoryScroll)
+		.AddGroup(B_VERTICAL, B_USE_HALF_ITEM_SPACING)
+			.Add(fAddButton)
+			.Add(fCancelButton)
+		.End()
+	.End();
+
+	BLayoutBuilder::Group<>(this, B_VERTICAL, 0)
+		.SetInsets(B_USE_WINDOW_SPACING)
+		.Add(fMainView)
+	.End();
+}
+
+
+void
+CategoryWindow::MessageReceived(BMessage* message)
+{
+	switch(message->what) {
+
+		default:
+			BWindow::MessageReceived(message);
+			break;
+	}
+}
+
+
+bool
+CategoryWindow::QuitRequested()
+{
+	return true;
+}
---
+++ src/CategoryWindow.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Akshay Agarwal <agarwal.akshay.akshay8@gmail.com>
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef CATEGORYWINDOW_H
+#define CATEGORYWINDOW_H
+
+
+#include <Button.h>
+#include <ListView.h>
+#include <ScrollView.h>
+#include <View.h>
+#include <Window.h>
+
+
+class CategoryWindow: public BWindow {
+public:
+			CategoryWindow();
+
+	virtual void	MessageReceived(BMessage* message);
+	virtual bool	QuitRequested();
+
+private:
+	static const uint32 kAddPressed			= 1000;
+	static const uint32 kCancelPressed		= 1001;
+	static const uint32 kCategorySelected	= 1002;
+
+	BView*			fMainView;
+	BListView*		fCategoryList;
+	BScrollView*	fCategoryScroll;
+	BButton*		fAddButton;
+	BButton*		fCancelButton;
+
+};
+
+
+#endif
