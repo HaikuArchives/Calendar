b386c0066dc48a021ecd02f73dba125923a2cdea
malbx <30420820+malbx@users.noreply.github.com>
2020-12-15 02:39:34 -0500

    add missing return statements and fix format specifier

--- src/plugin/GoogleCalendar/EventSync.cpp
+++ src/plugin/GoogleCalendar/EventSync.cpp
@@ -470,6 +470,8 @@ EventSync::AddEvent(Event* event)
 	if (Requests::Request(url, B_HTTP_POST, headers, NULL, &jsonString, reply)
 		== B_ERROR)
 		return B_ERROR;
+
+	return B_OK;
 }
 
 
@@ -484,6 +486,8 @@ EventSync::DeleteEvent(Event* event)
 	if (Requests::Request(endpoint, B_HTTP_DELETE, NULL, NULL, NULL, reply)
 		== B_ERROR)
 		return B_ERROR;
+
+	return B_OK;
 }
 
 
@@ -541,7 +545,7 @@ EventSync::RFC3339ToTime(const char* timeString, EventDateType type)
 	}
 
 	else if (static_cast<EventDateType>(type) == kEventUpdateDate) {
-		sscanf (timeString, "%d-%d-%dT%d:%d:%d.%d'Z'", &year, &month, &day,
+		sscanf (timeString, "%d-%d-%dT%d:%d:%d.%*d'Z'", &year, &month, &day,
            &hour, &minute, &second);
 	}
 
