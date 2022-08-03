ea80a0676253296c76e173373b8dbc3cc9a29f52
Harshit Sharma <harshits908@gmail.com>
2022-08-03 19:53:33 +0530

    Added ReminderEvent Class and now the proram loads the Events into a ReminderEventList

--- daemon/main/CalendarDaemon.cpp
+++ daemon/main/CalendarDaemon.cpp
@@ -5,15 +5,21 @@
 
 #include "CalendarDaemon.h"
 
+#include <DateTime.h>
+#include <Directory.h>
 #include <FindDirectory.h>
 #include <Notification.h>
 #include <Path.h>
 #include <iostream>
 #include <csignal>
+#include <time.h>
 
-#define EVENT_DIRECTORY	"config/settings/Calendar/events"
-#define START_ATTR		"Event:Start"
-#define DESCRIPTION_ATTR "Event:Description"
+#define EVENT_DIRECTORY		"config/settings/Calendar/events"
+#define START_ATTR			"Event:Start"
+#define DESCRIPTION_ATTR 	"Event:Description"
+#define PLACE_ATTR			"Event:Place"
+#define NAME_ATTR			"Event:Name"
+#define CATNAME_ATTR		"Event:Category"
 
 const char* kApplicationSignature = "application/x-vnd.CalendarDaemon";
 
@@ -27,45 +33,160 @@ void signalHandler(int signum)
 	exit(signum);
 }
 
+
 int main()
 {
 	signal(SIGINT, signalHandler);
 	
 	CalendarDaemon app;
-	app.SayHello("Harshit");
-	
-	while(1)
-	{
-		std::cout << "Going to Sleep...\n";
-		sleep(1);
-	}
 	
 	return(0);
 }
 
 
+/*! 
+	ReminderEvent Class Definitions
+*/
+
+
+ReminderEvent::ReminderEvent(const char* name, const char* place,
+	const char* description, const char* catName, time_t start)
+{
+	fName = BString(name);
+	fPlace = BString(place);
+	fDescription = BString(description);
+	fCatName = BString(catName);
+	fStart = start;
+}
+
+
+ReminderEvent::ReminderEvent(ReminderEvent &event)
+{
+	fName = event.GetName();
+	fPlace = event.GetPlace();
+	fDescription = event.GetDescription();
+	fCatName = event.GetCatName();
+	fStart = event.GetStartDateTime();
+}
+
+
+time_t
+ReminderEvent::GetStartDateTime() const
+{
+	return fStart;
+}
+
+
+void
+ReminderEvent::SetStartDateTime(time_t start)
+{
+	fStart = start;
+}
+
+
+const char*
+ReminderEvent::GetName() const
+{
+	return fName.String();
+}
+
+
+void
+ReminderEvent::SetName(const char* name)
+{
+	fName = BString(name);
+}
+
+
+const char*
+ReminderEvent::GetPlace() const
+{
+	return fPlace.String();
+}
+
+
+void
+ReminderEvent::SetPlace(const char* place)
+{
+	fPlace = BString(place);
+}
+
+
+const char*
+ReminderEvent::GetDescription() const
+{
+	return fDescription.String();
+}
+
+
+void
+ReminderEvent::SetDescription(const char* description)
+{
+	fDescription = BString(description);
+}
+
+
+const char*
+ReminderEvent::GetCatName() const
+{
+	return fCatName.String();
+}
+
+
+void
+ReminderEvent::SetCatName(const char* catName)
+{
+	fCatName = BString(catName);
+}
+
+
+
+/*! 
+	CalendarDaemon Class Definitions
+*/
+
+
 CalendarDaemon::CalendarDaemon()
 	:	BApplication(kApplicationSignature)
 {
+	std::cout << "Creating Daemon" << std::endl;
+	
+	fEventList = new ReminderEventList(20, true);
+	
 	BPath homeDir;
 	find_directory(B_USER_DIRECTORY, &homeDir);
 	
 	fEventDir = homeDir.Path();
 	fEventDir << "/" << EVENT_DIRECTORY;
 	
+	BDirectory directory(fEventDir.String());
+	if(directory.InitCheck() == B_ENTRY_NOT_FOUND)
+	{
+		std::cerr << "Failed to access the Events Directory" << std::endl;
+	}
+	else if(directory.InitCheck() == B_OK)
+	{
+		entry_ref ref;
+		while(directory.GetNextRef(&ref) == B_OK)
+			AddEventToList(&ref);
+	}
+	
 	BNotification notification(B_INFORMATION_NOTIFICATION);
-	notification.SetTitle("Calendar Daemon");
-	notification.SetContent(fEventDir);
+	notification.SetTitle("Found the Directory ;)");
+	notification.SetContent(fEventDir.String());
 	
 	notification.Send();
 	
 	std::cout << "Notification Sent!" << std::endl;
 }
 
+
 CalendarDaemon::~CalendarDaemon()
 {
-	std::cout << "Bye Bye" << std::endl;
+	std::cout << "Stopping Daemon, Clearing Memory, Good Bye! ;)" << std::endl;
+	delete(fEventList);
 }
+
 
 void
 CalendarDaemon::MessageReceived(BMessage *message)
@@ -81,13 +202,58 @@ CalendarDaemon::MessageReceived(BMessage *message)
 	}
 }
 
+
+void
+CalendarDaemon::AddEventToList(entry_ref* ref)
+{
+	ReminderEvent* event = _FileToReminderEvent(ref);
+	
+	std::cout << "\nEvent Name: " << event->GetName() << std::endl;
+	std::cout << "\nEvent Place: " << event->GetPlace() << std::endl;
+	
+	fEventList->AddItem(event);
+}
+
+
 bool
 CalendarDaemon::QuitRequested()
 {
 	return(true);
 }
 
-void CalendarDaemon::SayHello(const char* name)
+
+ReminderEvent*
+CalendarDaemon::_FileToReminderEvent(entry_ref* ref)
 {
-	std::cout << "Hello " << name << std::endl;
+	BNode node(ref);
+	BEntry entry(ref);
+	if (node.InitCheck() != B_OK || entry.InitCheck() != B_OK)
+		return NULL;
+
+	BString name = BString();
+	BString catName = BString();
+	BString desc = BString();
+	BString place = BString();
+	node.ReadAttrString(NAME_ATTR, &name);
+	node.ReadAttrString(CATNAME_ATTR, &catName);
+	node.ReadAttrString(DESCRIPTION_ATTR, &desc);
+	node.ReadAttrString(PLACE_ATTR, &place);
+
+	time_t start = time(NULL);
+	node.ReadAttr(START_ATTR, B_TIME_TYPE, 0, &start, sizeof(time_t));
+
+	return new ReminderEvent(name.String(), place.String(), desc.String(),
+		catName.String(), start);
+}
+
+
+int
+CalendarDaemon::_CompareFunction(ReminderEvent* a, ReminderEvent* b)
+{
+	if(difftime(a->GetStartDateTime(), b->GetStartDateTime()) < 0)
+		return(1);
+	else if(difftime(a->GetStartDateTime(), b->GetStartDateTime()) > 0)
+		return(-1);
+	else
+		return(0);
 }
--- daemon/main/CalendarDaemon.h
+++ daemon/main/CalendarDaemon.h
@@ -2,6 +2,7 @@
  * Copyright 2018, Your Name <your@email.address>
  * All rights reserved. Distributed under the terms of the MIT license.
  */
+
 #ifndef CALENDAR_DAEMON_H
 #define CALENDAR_DAEMON_H
 
@@ -21,6 +22,48 @@ class BMessage;
 class BString;
 
 
+/*!
+	ReminderEvent Class Declaration
+*/
+
+class ReminderEvent
+{
+public:
+	ReminderEvent(const char* name, const char* place,
+		const char* description, const char* catName, time_t start);
+	ReminderEvent(ReminderEvent& event);
+	
+	time_t		GetStartDateTime() const;
+	void		SetStartDateTime(time_t start);
+	
+	const char*	GetName() const;
+	void		SetName(const char* name);
+	
+	const char*	GetPlace() const;
+	void		SetPlace(const char* place);
+	
+	const char*	GetDescription() const;
+	void		SetDescription(const char* description);
+	
+	const char*	GetCatName() const;
+	void		SetCatName(const char* catName);
+	
+private:
+	BString		fName;
+	BString		fPlace;
+	BString		fDescription;
+	BString		fCatName;
+	
+	time_t		fStart;
+};
+
+typedef BObjectList<ReminderEvent> ReminderEventList;
+
+
+/*!
+	CalendarDaemon Class Declaration
+*/
+
 class CalendarDaemon : public BApplication
 {
 public:
@@ -30,16 +73,20 @@ public:
 	void		MessageReceived(BMessage* message);
 	bool		QuitRequested();
 	
-	void		SayHello(const char* name);
-	//void		Notify(Event* event);
+	void		Notify(Event* event);
+	void		AddEventToList(entry_ref* ref);
+	void		ShowEvent(entry_ref* ref);
 
 private:
 
-	BString		fEventDir;
+	ReminderEvent*		_FileToReminderEvent(entry_ref* ref);
+	int					_CompareFunction(ReminderEvent* a, ReminderEvent* b);
+
+	BString				fEventDir;
+	ReminderEventList*	fEventList;
 
 };
 
 int main();
 
-
-#endif // _H
+#endif
